60b5b9b7b93138d6d9c91549e9e2b899219bfb32
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-05-13 15:38:23 +0300

    Выносит футер в отдельный компонент

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Statistics from './components/statistics';
 import UserProfile from './components/user-profile';
 import Sort from './components/sort';
 import Filter from './components/filter';
+import Footer from './components/footer';
 import {getRandomFilms} from './films-data';
 
 const sort = new Sort();
@@ -15,9 +16,11 @@ const films = getRandomFilms(MAIN_FILMS_COUNT);
 
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
+const bodyElement = document.querySelector(`body`);
 
 const filter = new Filter(films);
 const pageController = new PageController(mainElement);
+const footer = new Footer(films);
 
 render(headerElement, userProfile);
 render(mainElement, filter);
@@ -26,3 +29,4 @@ render(mainElement, sort);
 pageController.render(films);
 
 render(mainElement, statistics);
+render(bodyElement, footer);
